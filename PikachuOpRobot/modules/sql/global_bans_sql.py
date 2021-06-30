c6021d7d4d125f06fbb48e54abee7fbda32457e8
LEGEND_OP <84610766+LEGEND-Xxxxx@users.noreply.github.com>
2021-06-30 13:13:42 +0530

    Update global_bans_sql.py

--- PikachuOpRobot/modules/sql/global_bans_sql.py
+++ PikachuOpRobot/modules/sql/global_bans_sql.py
@@ -1,8 +1,7 @@
 import threading
 
-from sqlalchemy import Column, UnicodeText, Integer, String, Boolean
-
 from PikachuOpRobot.modules.sql import BASE, SESSION
+from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 
 
 class GloballyBannedUsers(BASE):
@@ -23,7 +22,8 @@ class GloballyBannedUsers(BASE):
         return {
             "user_id": self.user_id,
             "name": self.name,
-            "reason": self.reason}
+            "reason": self.reason
+        }
 
 
 class GbanSettings(BASE):
@@ -140,8 +140,9 @@ def num_gbanned_users():
 def __load_gbanned_userid_list():
     global GBANNED_LIST
     try:
-        GBANNED_LIST = {x.user_id for x in SESSION.query(
-            GloballyBannedUsers).all()}
+        GBANNED_LIST = {
+            x.user_id for x in SESSION.query(GloballyBannedUsers).all()
+        }
     finally:
         SESSION.close()
 
@@ -149,8 +150,11 @@ def __load_gbanned_userid_list():
 def __load_gban_stat_list():
     global GBANSTAT_LIST
     try:
-        GBANSTAT_LIST = {x.chat_id for x in SESSION.query(
-            GbanSettings).all() if not x.setting}
+        GBANSTAT_LIST = {
+            x.chat_id
+            for x in SESSION.query(GbanSettings).all()
+            if not x.setting
+        }
     finally:
         SESSION.close()
 
